af37176a76fb5cd8d2cb636003acea5e053c8949
Aregus <74340447+Areg472@users.noreply.github.com>
2026-01-30 21:35:06 +0400

    added lyon

--- src/data/stations/index.js
+++ src/data/stations/index.js
@@ -3,12 +3,14 @@ import { tbilisi } from "./tbilisi";
 import { minsk } from "./minsk";
 import { london } from "./london";
 import { istanbul } from "./istanbul";
+import { lyon } from "./lyon";
 
 export const stations = {
   yerevan,
   tbilisi,
   minsk,
   london,
+  lyon,
   istanbul,
 };
 
