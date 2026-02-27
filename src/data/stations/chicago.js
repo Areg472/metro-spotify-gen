31b9ca6f83221becf6b80a0f29ffe0a3cbc4c674
Aregus <74340447+Areg472@users.noreply.github.com>
2026-02-27 17:33:40 +0400

    chicago and tehran time

---
+++ src/data/stations/chicago.js
@@ -0,0 +1,604 @@
+export const chicago = {
+  name: "Chicago",
+  country: "United States",
+  defaultConnectorSize: 60,
+  auto: true,
+  stations: {
+    redLine: {
+      color: "#c60c30",
+      howard: {
+        name: "Howard",
+      },
+      jarvis: {
+        name: "Jarvis",
+      },
+      morse: {
+        name: "Morse",
+      },
+      loyola: {
+        name: "Loyola",
+      },
+      granville: {
+        name: "Granville",
+      },
+      thorndale: {
+        name: "Thorndale",
+      },
+      brynMawr: {
+        name: "Bryn Mawr",
+      },
+      berwyn: {
+        name: "Berwyn",
+      },
+      argyle: {
+        name: "Argyle",
+      },
+      lawrence: {
+        name: "Lawrence",
+      },
+      wilson: {
+        name: "Wilson",
+      },
+      sheridan: {
+        name: "Sheridan",
+      },
+      addison: {
+        name: "Addison",
+      },
+      belmont: {
+        name: "Belmont",
+      },
+      fullerton: {
+        name: "Fullerton",
+      },
+      northClybourn: {
+        name: "North/Clybourn",
+      },
+      clarkDivision: {
+        name: "Clark/Division",
+      },
+      chicago: {
+        name: "Chicago",
+      },
+      grandRedLine: {
+        name: "Grand",
+      },
+      lakeRedLine: {
+        name: "Lake",
+      },
+      monroeRedLine: {
+        name: "Monroe",
+      },
+      jacksonRedLine: {
+        name: "Jackson",
+      },
+      harrison: {
+        name: "Harrison",
+      },
+      roosevelt: {
+        name: "Roosevelt",
+      },
+      cermakChinatown: {
+        name: "Cermak-Chinatown",
+      },
+      sox35th: {
+        name: "Sox-35th",
+      },
+      fortySeventhRedLine: {
+        name: "47th",
+      },
+      garfield: {
+        name: "Garfield",
+      },
+      sixtyThird: {
+        name: "63rd",
+      },
+      sixtyNinth: {
+        name: "69th",
+      },
+      seventyNinth: {
+        name: "79th",
+      },
+      eightySeventh: {
+        name: "87th",
+      },
+      ninetyFifthNintyFifthDanRyan: {
+        name: "95th/Dan Ryan",
+      },
+    },
+    blueLine: {
+      color: "#00a1de",
+      ohare: {
+        name: "O'Hare",
+      },
+      rosemont: {
+        name: "Rosemont",
+      },
+      cumberland: {
+        name: "Cumberland",
+      },
+      harlemOhare: {
+        name: "Harlem",
+      },
+      jeffersonPark: {
+        name: "Jefferson Park",
+      },
+      montrose: {
+        name: "Montrose",
+      },
+      irvingPark: {
+        name: "Irving Park",
+      },
+      addisonBlueLine: {
+        name: "Addison",
+      },
+      belmontBlueLine: {
+        name: "Belmont",
+      },
+      loganSquare: {
+        name: "Logan Square",
+      },
+      californiaBlue: {
+        name: "California",
+      },
+      westernBlueLine: {
+        name: "Western",
+      },
+      damen: {
+        name: "Damen",
+      },
+      divisionBlueLine: {
+        name: "Division",
+      },
+      chicagoBlueLine: {
+        name: "Chicago",
+      },
+      grandBlueLine: {
+        name: "Grand",
+      },
+      clarkLake: {
+        name: "Clark/Lake",
+      },
+      washingtonBlueLine: {
+        name: "Washington",
+      },
+      monroeBlueLine: {
+        name: "Monroe",
+      },
+      jacksonBlueLine: {
+        name: "Jackson",
+      },
+      lasalleVanBuren: {
+        name: "LaSalle/Van Buren",
+      },
+      clintonBlueLine: {
+        name: "Clinton",
+      },
+      uicHalsted: {
+        name: "UIC-Halsted",
+      },
+      racine: {
+        name: "Racine",
+      },
+      illinoisMedicalDistrict: {
+        name: "Illinois Medical District",
+      },
+      westernForestPark: {
+        name: "Western (Forest Park)",
+      },
+      kedziеHoman: {
+        name: "Kedzie-Homan",
+      },
+      pulaskiBlueLine: {
+        name: "Pulaski",
+      },
+      cicero: {
+        name: "Cicero",
+      },
+      austinBlueLine: {
+        name: "Austin",
+      },
+      oakPark: {
+        name: "Oak Park",
+      },
+      harlemForestPark: {
+        name: "Harlem (Forest Park)",
+      },
+      forestPark: {
+        name: "Forest Park",
+      },
+    },
+    brownLine: {
+      color: "#62361b",
+      kimball: {
+        name: "Kimball",
+      },
+      kedzie: {
+        name: "Kedzie",
+      },
+      francisco: {
+        name: "Francisco",
+      },
+      rockwell: {
+        name: "Rockwell",
+      },
+      westernBrownLine: {
+        name: "Western",
+      },
+      damenBrownLine: {
+        name: "Damen",
+      },
+      montroseBrownLine: {
+        name: "Montrose",
+      },
+      irvingParkBrownLine: {
+        name: "Irving Park",
+      },
+      addisonBrownLine: {
+        name: "Addison",
+      },
+      paulina: {
+        name: "Paulina",
+      },
+      southport: {
+        name: "Southport",
+      },
+      belmontBrownLine: {
+        name: "Belmont",
+      },
+      wellingtonBrownLine: {
+        name: "Wellington",
+      },
+      diversey: {
+        name: "Diversey",
+      },
+      fullertonBrownLine: {
+        name: "Fullerton",
+      },
+      armitage: {
+        name: "Armitage",
+      },
+      sedgwick: {
+        name: "Sedgwick",
+      },
+      chicagoArmitage: {
+        name: "Chicago",
+      },
+      merchandiseMart: {
+        name: "Merchandise Mart",
+      },
+      washingtonWells: {
+        name: "Washington/Wells",
+      },
+      quincy: {
+        name: "Quincy",
+      },
+      lasalleVanBurenBrown: {
+        name: "LaSalle/Van Buren",
+      },
+      libraryVanBuren: {
+        name: "Library-Van Buren",
+      },
+      adamsWabash: {
+        name: "Adams/Wabash",
+      },
+      madisonWabash: {
+        name: "Madison/Wabash",
+      },
+      stateLake: {
+        name: "State/Lake",
+      },
+      clarkLakeBrown: {
+        name: "Clark/Lake",
+      },
+    },
+    greenLine: {
+      color: "#009b3a",
+      harlemLake: {
+        name: "Harlem/Lake",
+      },
+      oakParkGreen: {
+        name: "Oak Park",
+      },
+      ridgeland: {
+        name: "Ridgeland",
+      },
+      austinGreen: {
+        name: "Austin",
+      },
+      central: {
+        name: "Central",
+      },
+      laramie: {
+        name: "Laramie",
+      },
+      ciceroGreen: {
+        name: "Cicero",
+      },
+      pulaskiGreen: {
+        name: "Pulaski",
+      },
+      conservatoryCentralParkDrive: {
+        name: "Conservatory-Central Park Drive",
+      },
+      kedzie: {
+        name: "Kedzie",
+      },
+      californiaGreen: {
+        name: "California",
+      },
+      ashland: {
+        name: "Ashland",
+      },
+      morganGreen: {
+        name: "Morgan",
+      },
+      clintonGreen: {
+        name: "Clinton",
+      },
+      clarkLakeGreen: {
+        name: "Clark/Lake",
+      },
+      stateLakeGreen: {
+        name: "State/Lake",
+      },
+      washingtonWabash: {
+        name: "Washington/Wabash",
+      },
+      adamsWabashGreen: {
+        name: "Adams/Wabash",
+      },
+      rooseveltGreen: {
+        name: "Roosevelt",
+      },
+      branches: [
+        {
+          branchedStation: "Roosevelt",
+          color: "#009b3a",
+          stations: {
+            cermak: {
+              name: "Cermak-McCormick Place",
+            },
+            thirtyFifthBronzeville: {
+              name: "35th-Bronzeville-IIT",
+            },
+            indianaGreen: {
+              name: "Indiana",
+            },
+            fortyThirdGreen: {
+              name: "43rd",
+            },
+            fortySeventhGreen: {
+              name: "47th",
+            },
+            fiftyFirstGreen: {
+              name: "51st",
+            },
+            garfieldGreen: {
+              name: "Garfield",
+            },
+            kingDrive: {
+              name: "King Drive",
+            },
+            cottageGrove: {
+              name: "Cottage Grove",
+            },
+          },
+        },
+        {
+          branchedStation: "Roosevelt",
+          color: "#009b3a",
+          stations: {
+            cermakMcCormickPlace: {
+              name: "Cermak-McCormick Place",
+            },
+            thirtyFifthBronzeville: {
+              name: "35th-Bronzeville-IIT",
+            },
+            indianaAshland: {
+              name: "Indiana",
+            },
+            halstedAshland: {
+              name: "Halsted",
+            },
+            ashlandAshland: {
+              name: "Ashland/63rd",
+            },
+          },
+        },
+      ],
+    },
+    orangeLine: {
+      color: "#f9461c",
+      midway: {
+        name: "Midway",
+      },
+      pulaskiOrange: {
+        name: "Pulaski",
+      },
+      kedzie: {
+        name: "Kedzie",
+      },
+      westernOrange: {
+        name: "Western",
+      },
+      thirtyFifthArcher: {
+        name: "35th/Archer",
+      },
+      ashlandOrange: {
+        name: "Ashland",
+      },
+      halsted: {
+        name: "Halsted",
+      },
+      rooseveltOrange: {
+        name: "Roosevelt",
+      },
+      libraryVanBurenOrange: {
+        name: "Library-Van Buren",
+      },
+      lasalleVanBurenOrange: {
+        name: "LaSalle/Van Buren",
+      },
+      quincyOrange: {
+        name: "Quincy",
+      },
+      washingtonWellsOrange: {
+        name: "Washington/Wells",
+      },
+      clarkLakeOrange: {
+        name: "Clark/Lake",
+      },
+      stateLakeOrange: {
+        name: "State/Lake",
+      },
+      madisonWabashOrange: {
+        name: "Madison/Wabash",
+      },
+      adamsWabashOrange: {
+        name: "Adams/Wabash",
+      },
+    },
+    pinkLine: {
+      color: "#e27ea6",
+      fiftyFourthCermak: {
+        name: "54th/Cermak",
+      },
+      ciceroPink: {
+        name: "Cicero",
+      },
+      kostner: {
+        name: "Kostner",
+      },
+      pulaskiPink: {
+        name: "Pulaski",
+      },
+      centralPark: {
+        name: "Central Park",
+      },
+      kedzie: {
+        name: "Kedzie",
+      },
+      californiaPink: {
+        name: "California",
+      },
+      westernPink: {
+        name: "Western",
+      },
+      damenPink: {
+        name: "Damen",
+      },
+      eighteenth: {
+        name: "18th",
+      },
+      polkPink: {
+        name: "Polk",
+      },
+      ashlandPink: {
+        name: "Ashland",
+      },
+      morganPink: {
+        name: "Morgan",
+      },
+      clintonPink: {
+        name: "Clinton",
+      },
+      clarkLakePink: {
+        name: "Clark/Lake",
+      },
+      stateLakePink: {
+        name: "State/Lake",
+      },
+      washingtonWabashPink: {
+        name: "Washington/Wabash",
+      },
+      adamsWabashPink: {
+        name: "Adams/Wabash",
+      },
+    },
+    purpleLine: {
+      color: "#522398",
+      linden: {
+        name: "Linden",
+      },
+      centralPurple: {
+        name: "Central",
+      },
+      noyes: {
+        name: "Noyes",
+      },
+      fosterPurple: {
+        name: "Foster",
+      },
+      davis: {
+        name: "Davis",
+      },
+      dempster: {
+        name: "Dempster",
+      },
+      mainPurple: {
+        name: "Main",
+      },
+      southBoulevard: {
+        name: "South Boulevard",
+      },
+      howardPurple: {
+        name: "Howard",
+      },
+      jarvisPurple: {
+        name: "Jarvis",
+      },
+      morsePurple: {
+        name: "Morse",
+      },
+      loyolaPurple: {
+        name: "Loyola",
+      },
+      granvillePurple: {
+        name: "Granville",
+      },
+      thorndalePurple: {
+        name: "Thorndale",
+      },
+      brynMawrPurple: {
+        name: "Bryn Mawr",
+      },
+      berwynPurple: {
+        name: "Berwyn",
+      },
+      argylePurple: {
+        name: "Argyle",
+      },
+      lawrencePurple: {
+        name: "Lawrence",
+      },
+      wilsonPurple: {
+        name: "Wilson",
+      },
+      sheridanPurple: {
+        name: "Sheridan",
+      },
+      addisonPurple: {
+        name: "Addison",
+      },
+      belmontPurple: {
+        name: "Belmont",
+      },
+      fullertonPurple: {
+        name: "Fullerton",
+      },
+    },
+    yellowLine: {
+      color: "#f9e300",
+      dempsterSkokie: {
+        name: "Dempster-Skokie",
+      },
+      oakton: {
+        name: "Oakton-Skokie",
+      },
+      howardYellow: {
+        name: "Howard",
+      },
+    },
+  },
+};
---
+++ src/data/stations/tehran.js
@@ -0,0 +1,443 @@
+export const tehran = {
+  name: "Tehran",
+  country: "Iran",
+  defaultConnectorSize: 60,
+  auto: true,
+  stations: {
+    lineOne: {
+      color: "#e30613",
+      tajrish: {
+        name: "Tajrish",
+      },
+      gheytarieh: {
+        name: "Gheytarieh",
+      },
+      shahidSadr: {
+        name: "Shahid Sadr",
+      },
+      shahidMadani: {
+        name: "Shahid Madani",
+      },
+      sahebQaraniyeh: {
+        name: "Saheb Qaraniyeh",
+      },
+      mirdamad: {
+        name: "Mirdamad",
+      },
+      mosalla: {
+        name: "Mosalla",
+      },
+      tabiat: {
+        name: "Tabiat",
+      },
+      enghelabEslami: {
+        name: "Enghelab-e-Eslami",
+      },
+      khayyam: {
+        name: "Khayyam",
+      },
+      imamKhomeiniLineOne: {
+        name: "Imam Khomeini",
+      },
+      panzdahKhordad: {
+        name: "Panzdah-e-Khordad",
+      },
+      darvazehDowlatLineOne: {
+        name: "Darvazeh Dowlat",
+      },
+      sangalaj: {
+        name: "Sangalaj",
+      },
+      mellat: {
+        name: "Mellat",
+      },
+      emamHossein: {
+        name: "Emam Hossein",
+      },
+      darvazehShahr: {
+        name: "Darvazeh Shahr",
+      },
+      tehranPars: {
+        name: "Tehran Pars",
+      },
+      farhangsara: {
+        name: "Farhangsara",
+      },
+      shahidBagheri: {
+        name: "Shahid Bagheri",
+      },
+      beryanak: {
+        name: "Beryanak",
+      },
+      ghazalieh: {
+        name: "Ghazalieh",
+      },
+      shahedShahr: {
+        name: "Shahed Shahr",
+      },
+      shahreRey: {
+        name: "Shahr-e-Rey",
+      },
+      kolahdooz: {
+        name: "Kolahdooz",
+      },
+      shahedBeheshti: {
+        name: "Shahed Beheshti",
+      },
+      dorsapey: {
+        name: "Dorsapey",
+      },
+      abshar: {
+        name: "Abshar",
+      },
+      kahrizak: {
+        name: "Kahrizak",
+      },
+    },
+    lineTwo: {
+      color: "#003590",
+      sadeghiyeh: {
+        name: "Sadeghiyeh",
+      },
+      shademan: {
+        name: "Shademan",
+      },
+      meydanAzadi: {
+        name: "Meydan Azadi",
+      },
+      darvazehShemiranLineTwo: {
+        name: "Darvazeh Shemiran",
+      },
+      tehranSaderat: {
+        name: "Tehran Saderat",
+      },
+      arjantin: {
+        name: "Arjantin",
+      },
+      tohid: {
+        name: "Tohid",
+      },
+      shahidMohammadSoltani: {
+        name: "Shahid Mohammad Soltani",
+      },
+      imamKhomeiniLineTwo: {
+        name: "Imam Khomeini",
+      },
+      khaleGhalibaf: {
+        name: "Khale Ghalibaf",
+      },
+      darvazehDowlatLineTwo: {
+        name: "Darvazeh Dowlat",
+      },
+      shahidHasheminejad: {
+        name: "Shahid Hasheminejad",
+      },
+      nematAbad: {
+        name: "Nemat Abad",
+      },
+      khavaran: {
+        name: "Khavaran",
+      },
+      narmak: {
+        name: "Narmak",
+      },
+      saraAbad: {
+        name: "Sara Abad",
+      },
+      pirozi: {
+        name: "Pirozi",
+      },
+      tehranNoLineTwo: {
+        name: "Tehran-No",
+      },
+      bimehLineTwo: {
+        name: "Bimeh",
+      },
+    },
+    lineThree: {
+      color: "#49bcd8",
+      qaem: {
+        name: "Qaem",
+      },
+      shahidMohammadi: {
+        name: "Shahid Mohammadi",
+      },
+      eqdasiya: {
+        name: "Eqdasiya",
+      },
+      nobonyad: {
+        name: "Nobonyad",
+      },
+      hosseinAbad: {
+        name: "Hossein Abad",
+      },
+      meydanHeravi: {
+        name: "Meydan Heravi",
+      },
+      shahidZeynaldin: {
+        name: "Shahid Zeynaldin",
+      },
+      khwajaAbdullahAnsari: {
+        name: "Khwaja Abdullah Ansari",
+      },
+      shahidSayyadShirazi: {
+        name: "Shahid Sayyad Shirazi",
+      },
+      shahidGhodusi: {
+        name: "Shahid Ghodusi",
+      },
+      sohravardi: {
+        name: "Sohravardi",
+      },
+      shahidBeheshtiLineThree: {
+        name: "Shahid Beheshti",
+      },
+      mirzaeiShirazi: {
+        name: "Mirzaei Shirazi",
+      },
+      meydanJihad: {
+        name: "Meydan Jihad",
+      },
+      meydanHazratValiAsr: {
+        name: "Meydan Hazrat Vali Asr",
+      },
+      tatrShahr: {
+        name: "Tatr Shahr",
+      },
+      moniriyeh: {
+        name: "Moniriyeh",
+      },
+      mahdieh: {
+        name: "Mahdieh",
+      },
+      rahAhan: {
+        name: "Rah Ahan",
+      },
+      javadiyeh: {
+        name: "Javadiyeh",
+      },
+      zamzam: {
+        name: "Zamzam",
+      },
+      shahrakShariati: {
+        name: "Shahrak Shariati",
+      },
+      abdolAbad: {
+        name: "Abdol Abad",
+      },
+      nematAbadLineThree: {
+        name: "Nemat Abad",
+      },
+      azadegan: {
+        name: "Azadegan",
+      },
+    },
+    lineFour: {
+      color: "#fdd000",
+      shahidKolahdooz: {
+        name: "Shahid Kolahdooz",
+      },
+      yadegarEmam: {
+        name: "Yadegar-e-Emam",
+      },
+      shahidNurbakhsh: {
+        name: "Shahid Nurbakhsh",
+      },
+      mirdamadLineFour: {
+        name: "Mirdamad",
+      },
+      bimehLineFour: {
+        name: "Bimeh",
+      },
+      tohidLineFour: {
+        name: "Tohid",
+      },
+      azadiLineFour: {
+        name: "Azadi",
+      },
+      meydanAzadiLineFour: {
+        name: "Meydan Azadi",
+      },
+      darvazehShemiranLineFour: {
+        name: "Darvazeh Shemiran",
+      },
+      teatrShahr: {
+        name: "Teatr-e-Shahr",
+      },
+      ferdowsi: {
+        name: "Ferdowsi",
+      },
+      shahdShahriari: {
+        name: "Shahd Shahriari",
+      },
+      emamKhomeiniLineFour: {
+        name: "Emam Khomeini",
+      },
+      shahrekord: {
+        name: "Shahrekord",
+      },
+      abyari: {
+        name: "Abyari",
+      },
+      shahidMottahari: {
+        name: "Shahid Mottahari",
+      },
+      peykanshahr: {
+        name: "Peykanshahr",
+      },
+      islamshahr: {
+        name: "Islamshahr",
+      },
+      erameSabz: {
+        name: "Eram-e-Sabz",
+      },
+    },
+    lineSix: {
+      color: "#d685bb",
+      dolatAbad: {
+        name: "Dolat Abad",
+      },
+      shahidRahimi: {
+        name: "Shahid Rahimi",
+      },
+      shahidNazari: {
+        name: "Shahid Nazari",
+      },
+      tarasht: {
+        name: "Tarasht",
+      },
+      meydanResalatLineSix: {
+        name: "Meydan Resalat",
+      },
+      shahidHaqqani: {
+        name: "Shahid Haqqani",
+      },
+      bostandoBlvdLineSix: {
+        name: "Bostando Blvd",
+      },
+      emamHussein: {
+        name: "Emam Hussein",
+      },
+      shahidMadaniLineSix: {
+        name: "Shahid Madani",
+      },
+      hafteTir: {
+        name: "Hafte Tir",
+      },
+      emamaZadeMahruq: {
+        name: "Emamzadeh Mahruq",
+      },
+      emamzadehYahya: {
+        name: "Emamzadeh Yahya",
+      },
+      vafadar: {
+        name: "Vafadar",
+      },
+      parkWayLineSix: {
+        name: "Park Way",
+      },
+      fath: {
+        name: "Fath",
+      },
+      baqershahr: {
+        name: "Baqershahr",
+      },
+      emamzadehEbrahim: {
+        name: "Emamzadeh Ebrahim",
+      },
+      shamsAbadLineSix: {
+        name: "Shams Abad",
+      },
+      shahreReyLineSix: {
+        name: "Shahr-e-Rey",
+      },
+      khorramdasht: {
+        name: "Khorramdasht",
+      },
+      safiAlishah: {
+        name: "Safi Alishah",
+      },
+      dowlatKhah: {
+        name: "Dowlat Khah",
+      },
+      chardowli: {
+        name: "Chardowli",
+      },
+      fadak: {
+        name: "Fadak",
+      },
+      daneshgahOlum: {
+        name: "Daneshgah Olum",
+      },
+      hazratAbdolAzimLineSix: {
+        name: "Hazrat Abdol Azim",
+      },
+    },
+    lineSeven: {
+      color: "#672f8a",
+      meydanSanat: {
+        name: "Meydan-e-San'at",
+      },
+      shahidQandiharLineSeven: {
+        name: "Shahid Qandihar",
+      },
+      bostandoBlvdLineSeven: {
+        name: "Bostando Blvd",
+      },
+      bostandarLineSeven: {
+        name: "Bostandar",
+      },
+      emamAliLineSeven: {
+        name: "Emam Ali",
+      },
+      meydanResalatLineSeven: {
+        name: "Meydan Resalat",
+      },
+      bimehLineSeven: {
+        name: "Bimeh",
+      },
+      mehrAbadAirportLineSeven: {
+        name: "Mehrabad Airport",
+      },
+      azadiLineSeven: {
+        name: "Azadi",
+      },
+      shahidNawabeSafavi: {
+        name: "Shahid Nawab-e-Safavi",
+      },
+      meydanMollasadra: {
+        name: "Meydan Mollasadra",
+      },
+      shahidHemmat: {
+        name: "Shahid Hemmat",
+      },
+      babayiHighway: {
+        name: "Babayi Highway",
+      },
+      meydanValiasr: {
+        name: "Meydan Valiasr",
+      },
+      shahidKolahdouzLineSeven: {
+        name: "Shahid Kolahdouz",
+      },
+      meydanShohada: {
+        name: "Meydan Shohada",
+      },
+      tehranNoLineSeven: {
+        name: "Tehran-No",
+      },
+      meydanMohammadiyeh: {
+        name: "Meydan Mohammadiyeh",
+      },
+      parkWayLineSeven: {
+        name: "Park Way",
+      },
+      shamsAbadLineSeven: {
+        name: "Shams Abad",
+      },
+      basij: {
+        name: "Basij",
+      },
+    },
+  },
+};
