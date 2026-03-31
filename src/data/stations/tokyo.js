46912ac296b0a0cdff5077582fd52acc8520beab
Areg472 <aregazatyank@outlook.com>
2026-03-31 15:02:36 +0400

    hmmmmm

--- src/data/stations/tokyo.js
+++ src/data/stations/tokyo.js
@@ -472,7 +472,7 @@ export const tokyo = {
       aoyamaItchomeHanzomon: {
         name: "Aoyama-itchome",
       },
-      nagatacho: {
+      nagatachoOrange: {
         name: "Nagatacho",
       },
       hanzomon: {
