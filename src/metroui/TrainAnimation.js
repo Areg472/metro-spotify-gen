7ae09f5855f2b41fb507193da038f524bd7feaf8
Areg472 <aregazatyank@outlook.com>
2026-04-11 12:01:03 +0400

    hmmm

--- src/metroui/TrainAnimation.js
+++ src/metroui/TrainAnimation.js
@@ -19,7 +19,7 @@ export function TrainAnimation({ pathCoords, connectorSize, color }) {
 
   useEffect(() => {
     startTimeRef.current = null;
-    setProgress(0);
+    progressRef.current = 0;
 
     const animate = (timestamp) => {
       if (!startTimeRef.current) startTimeRef.current = timestamp;
